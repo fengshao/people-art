94f35964fa018a4876e0f9bf6189bc3d63b078da
xiaojinfeng <fengshao74110@126.com>
2017-11-01 21:51:44 +0800

    修改body的宽高

--- src/component/loading/index.js
+++ src/component/loading/index.js
@@ -9,13 +9,12 @@ var Gallery = React.createClass({
 
 	render() {
 		//<div className="loading-title">{this.props.percent} %</div>
-
+		// <div className="loading-title">正在读取数据</div>
 		var _this = this;
 		return (
 			<div className="img-loading-content">
 				<div className="loading-content">
 					<div className="test-content">
-						<div className="loading-title">正在读取数据</div>
 						<div className="loader-inner pacman loading-title">
 							<div></div>
 							<div></div>
